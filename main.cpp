e404b3d0114bbfc3ff6b60aefbf0576b23566e19
maxisusi <maxbalej@proton.me>
2025-01-28 07:10:30 +0000

    refaco server socket

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
     exit(EXIT_FAILURE);                                                        \
   } while (0)
 
+#define PORT 9500
+
 int main() {
 
   int opt = 1;
@@ -20,9 +22,11 @@ int main() {
   int server_socket = socket(AF_INET, SOCK_STREAM, 0);
   setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
 
-  sockaddr_in server_address;
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(9500);
+  sockaddr_in server_address = {
+      .sin_family = AF_INET,
+      .sin_port = htons(PORT),
+  };
+
   server_address.sin_addr.s_addr = INADDR_ANY;
 
   if (bind(server_socket, (struct sockaddr *)&server_address,
@@ -32,17 +36,21 @@ int main() {
   if (listen(server_socket, 5) == -1)
     handle_error("listen");
 
+  std::cout << "Listening on port: " << PORT << std::endl;
+
   while (true) {
+
     int client_socket = accept(server_socket, nullptr, nullptr);
     char buffer[1024] = {0};
 
     recv(client_socket, buffer, sizeof(buffer), 0);
-
     std::cout << "Message from client: " << buffer << std::endl;
+
     if (write(client_socket, buffer, sizeof(buffer)) == -1)
       handle_error("write");
   }
 
   close(server_socket);
+
   return 0;
 }
